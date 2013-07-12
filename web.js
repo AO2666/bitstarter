0609161ea310ad5af1cff05c4c97c55ff891e6a9
Antonio Ortega <antonio.ortega@gmail.com>
2013-07-12 16:41:24 +0000

    2nd attempt web.js

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 //var buffer_text = new Buffer( 100);
 
-var file_buffer = new Buffer;
+var file_buffer = new Buffer( 1000);
 
-file_buffer = fs.readFile(index.html); file
+file_buffer = fs.readFile('index.html'); 
 
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
