7ffa361220df3a0627b1b4fc4e5869cbf6e0cda1
Antonio Ortega <antonio.ortega@gmail.com>
2013-07-20 16:50:48 +0000

    final

--- grader.js
+++ grader.js
@@ -29,6 +29,11 @@ var rest = require('restler');
 
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://google.com";
+
+var urlfile = "test.html"; 
+
+// rest.get(apiurl).on('complete', response2console);
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -67,9 +72,23 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to html file', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_link>', 'URL of html file', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if(program.url) {
+	rest.get(program.url).on('complete', function(result)
+				 {
+				     fs.writeFileSync(urlfile,result);
+				 });
+//	console.log("URL works");
+	var checkJson = checkHtmlFile(urlfile, program.checks);
+    }
+    else if (program.file) {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+    }
+    else {
+	console.log("Please include a file or an url as parameter.");
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
